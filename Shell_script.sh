8c5ef23a454530262198e5b88f22a5d17eb170df
Nicolas Bailly <nicolasbailly65@gmail.com>
2025-10-13 16:28:27 -0400

    Update Shell_script.sh

--- Shell_script.sh
+++ Shell_script.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
-# Load sra-tools module
-module load sra-toolkit/3.0.9
+# ================================================
+# Project: Raw Data Download, Assembly, and AMR Gene Detection
+# Description: 
+#   This script downloads raw sequencing data from NCBI SRA,
+#   converts to FASTQ, performs genome assembly using SPAdes,
+#   and detects antibiotic resistance genes using Abricate.
+# ================================================ 
 
+#_ Set up environment ------------
+module load sra-toolkit/3.0.9
+module load spades/4.2.0
+module load StdEnv/2020
+module load gcc/9.3.0
+module load  abricate/1.0.0
+
+mkdir -p assemblies
+# Make sure output directory exists 
+
+#_ Download raw data (.sra files) ------------
 echo "Downloading .sra files..."
 prefetch SRR35773224
 prefetch SRR35773225
@@ -10,6 +26,8 @@ prefetch SRR35773226
 prefetch SRR35773227
 prefetch SRR35773228
 echo "All downloads complete."
+
+#_Convert .sra Files to FASTQ Format ------------
 
 echo "Converting .sra files to FASTQ..."
 fasterq-dump SRR35773224
@@ -19,13 +37,8 @@ fasterq-dump SRR35773227
 fasterq-dump SRR35773228
 echo "Conversion complete."
 
-# Load SPAdes module
-module load spades/4.2.0
+#_Genome Assembly for Each Sample ------------
 
-# Make sure output directory exists
-mkdir -p assemblies
-
-# Assemble each strain separately
 spades.py \
   -1 SRR35773224_1.fastq \
   -2 SRR35773224_2.fastq \
@@ -58,14 +71,7 @@ spades.py \
 
 echo "All assemblies complete!"
 
-####Load prerequisite module
-module load StdEnv/2020
-module load gcc/9.3.0
-
-####Load abricate module
-module load  abricate/1.0.0
-
-#####Determining the detect known antibiotic resistant genes
+#_Detect Known Antibiotic Resistance Genes using Abricate ------------
 
 echo 'Running abricate for SRR35773224'
 abricate --db  resfinder SRR35773224 > SRR35773224.txt
@@ -81,6 +87,7 @@ abricate --db resfinder SRR35773227 > SRR35773227.txt
 
 echo 'Running abricade for SRR35773228'
 abricate --db resfinder SRR35773228  > SRR35773228.txt
+
 
 
 
